8cfd1cc910cee6c763a97cbcedffae566b9bd114
Ivan Silvestre <31860787+ivansilvestre@users.noreply.github.com>
2024-03-21 18:02:30 +0000

    Create fedora-maintenance.sh

---
+++ fedora-maintenance.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# ----------------------------- VARIABLES ----------------------------- #
+
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+NOCOLOR='\033[0m'
+
+# ----------------------------- FUNCTIONS LOGIC ----------------------------- #
+
+update () {
+    echo
+        echo -e "${BLUE} .................. Starting updates................. ${NOCOLOR}"
+    echo
+        sudo dnf update && sudo dnf upgrade -y
+    echo
+        echo -e "${BLUE} ---------------------------------------------------- ${NOCOLOR}"
+    echo
+        flatpak update
+}
+
+cleaning () { 
+    echo    
+        echo -e "${BLUE} ..................... Cleaning ..................... ${NOCOLOR}"
+    echo
+        sudo dnf autoremove && sudo dnf clean all -y
+    echo
+        echo -e "${BLUE} ..................... All Done ..................... ${NOCOLOR}" 
+}
+
+options (){
+    if [[ $answer == 'r' ]]
+        then
+        reboot
+    elif [[ $answer == 'p' ]]
+        then
+        poweroff  
+    else
+        echo -e "${BLUE} ---------------------------------------------------- ${NOCOLOR}"
+        echo
+    fi
+}
+
+# ----------------------------- FUNCTIONS ----------------------------- #
+
+update
+
+cleaning
+
+echo
+echo -e "${GREEN}
+    What do you want to do? 
+
+    ${NOCOLOR}Reboot${GREEN}: press ${NOCOLOR}r${GREEN} and ${NOCOLOR}Enter${GREEN};
+    ${NOCOLOR}Poweroff${GREEN}: press ${NOCOLOR}p${GREEN} and ${NOCOLOR}Enter${GREEN};
+    ${NOCOLOR}Stay in command Line${GREEN}: press ${NOCOLOR}Enter${GREEN}; 
+    ${NOCOLOR}"
+    
+read answer;
+
+options
